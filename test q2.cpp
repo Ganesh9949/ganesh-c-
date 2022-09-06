1f8baf43a0b2d5804200647b12db0b4d12bbb5d1
Ganesh9949 <112487424+Ganesh9949@users.noreply.github.com>
2022-09-06 09:25:44 +0530

    Add files via upload

---
+++ test q2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter a positive integer";
+	cin>>n;
+	for(inti=1; i<=10; ++i){
+		cout<<n<<"*"<<i<<"="<<n*i<<end;
+	}
+	return 0;
+}
