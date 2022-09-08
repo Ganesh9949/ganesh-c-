70763be9693b03bb5be50252c94b211bf7ae9a1d
Ganesh9949 <112487424+Ganesh9949@users.noreply.github.com>
2022-09-08 10:02:11 +0530

    Add files via upload

---
+++ test5 q1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class degree 
+public:
+	void name()
+	{
+		cout<<"i got a degree";
+	}
+	char:undergraduate public:degree
+	public:
+		undergraduate()
+		{
+			cout<<"i got undergraduate degree";
+		}
+		char:postgraduate public:degree
+		public:
+			postgraduate()
+			{
+				cout<<"i got postgraduate degree";
+			}
+			int main()
+			{
+				obj got deree;
+				obj1:undergraduate degree;
+				obj2:postgraduate degree;
+				return 0;
+			}
