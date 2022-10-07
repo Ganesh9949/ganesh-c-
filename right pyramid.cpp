a1262f3011d0dacf2cf64a8c6a51d82d18624295
Ganesh9949 <112487424+Ganesh9949@users.noreply.github.com>
2022-10-07 09:47:20 +0530

    Add files via upload

--- right pyramid.cpp
+++ right pyramid.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main()
 {
 	int i,j,n;
-	cout<<"enter number of rows ";
+	cout<<"enter number of rows to print";
 	cin>>n;
-	for(i=n;i>=1;i--){
+	for(i=1;i<=n;i++){
 		for (j=1;j<=i;j++){
 			cout<<"*";
 		}
